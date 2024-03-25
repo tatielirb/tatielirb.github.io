9b20248d773e7f333cd5d7fe31208753da8c9149
tatielirb <tatielirb@gmail.com>
2024-03-25 15:42:29 -0500

    feat: created datas and organized imgs

---
+++ src/util/DataMenu.ts
@@ -0,0 +1,46 @@
+export const menuSite = [
+  {
+    labelLink: 'Portfolio',
+    link: '',
+  },
+  {
+    labelLink: 'About Me',
+    link: '',
+  },
+  {
+    labelLink: 'Skills',
+    link: '',
+  },
+  {
+    labelLink: 'What I do',
+    link: '',
+  },
+]
+
+export const menuSocialMidas = [
+  {
+    platform: 'Linkefin',
+    icon: '',
+    link: 'https://www.linkedin.com/in/tatielirb/',
+  },
+  {
+    platform: 'Twitter',
+    icon: '',
+    link: 'https://twitter.com/tatielirb',
+  },
+  {
+    platform: 'Behance',
+    icon: '',
+    link: 'https://www.behance.net/tatielirb',
+  },
+  {
+    platform: 'Github',
+    icon: '',
+    link: 'https://github.com/tatielirb',
+  },
+  {
+    platform: 'Medium',
+    icon: '',
+    link: 'https://medium.com/@tatielirb',
+  },
+]
