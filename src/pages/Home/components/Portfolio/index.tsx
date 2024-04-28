ccd0dd6d014a0acb63ab48acee6ebc8c020086da
tatielirb <tatielirb@gmail.com>
2024-04-28 19:28:50 -0300

    feat:created scrit and button for loading portfolio

--- src/pages/Home/components/Portfolio/index.tsx
+++ src/pages/Home/components/Portfolio/index.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
 import Title from 'components/Title'
 import Card from 'components/Card'
 import { customers } from 'utils/DataCustomer'
 import './portfolio.scss'
 
 export default function Portfolio() {
+  const [visibleCount, setVisibleCount] = useState(12)
+
+  const handleLoadMore = () => {
+    if (visibleCount >= customers.length) {
+      setVisibleCount(12)
+    } else {
+      setVisibleCount(visibleCount + 12)
+    }
+  }
+
+  const visibleCustomers = customers.slice(0, visibleCount)
+
   return (
     <div className="portfolio" id="portfolio">
       <Title
@@ -12,9 +25,15 @@ export default function Portfolio() {
         styleColor="blue"
       />
 
-      {customers.map((item) => (
-        <Card {...item} />
+      {visibleCustomers.map((item, index) => (
+        <Card key={index} {...item} />
       ))}
+
+      <div className="row col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 justify-content-md-center box-button">
+        <button className="btn load-more" onClick={handleLoadMore}>
+          {visibleCount >= customers.length ? 'Load Less' : 'Load More'}
+        </button>
+      </div>
     </div>
   )
 }
