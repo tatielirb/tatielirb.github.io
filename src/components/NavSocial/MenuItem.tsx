8776a313fe6cb3706e537962ed8c2848eb626183
tatielirb <tatielirb@gmail.com>
2026-01-16 10:50:48 +0100

    feat: added noreferrer social link

--- src/components/NavSocial/MenuItem.tsx
+++ src/components/NavSocial/MenuItem.tsx
@@ -2,7 +2,7 @@ import { MenuItemProps } from 'types/components/Nav'
 export default function MenuItem({ name, href, icon }: MenuItemProps) {
   return (
     <li>
-      <a href={href} target="_blank">
+      <a href={href} target="_blank" rel="noreferrer">
         <p>{name}</p>
         <i className={`bi bi-${icon}`}></i>
       </a>
